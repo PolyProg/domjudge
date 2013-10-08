21e1361dd05b602dd278362a93d39807f4161210
Robert R. Enderlein <robertrichard.enderlein@gmail.com>
2013-10-08 20:08:27 +0200

    Removed GMP from compilation flags for C/C++

--- judge/compile_c.sh
+++ judge/compile_c.sh
@@ -8,12 +8,12 @@ DEST="$2"
 LIBRARY="$4"
 #STATIC="-static"
 
-echo gcc -Wall -O2 $STATIC -pipe -o $DEST $SOURCE $LIBRARY -lm -lgmp
+echo gcc -Wall -O2 $STATIC -pipe -o $DEST $SOURCE $LIBRARY -lm
 # -Wall:	Report all warnings
 # -O2:		Level 2 optimizations (default for speed)
 # -static:	Static link with all libraries
 # -pipe:	Use pipes for communication between stages of compilation
 # -lm:		Link with math-library (has to be last argument!)
 # -lgmp:        Link with GNU multiprecision library
-gcc -Wall -O2 $STATIC -pipe -o $DEST $SOURCE $LIBRARY -lm -lgmp
+gcc -Wall -O2 $STATIC -pipe -o $DEST $SOURCE $LIBRARY -lm
 exit $?
