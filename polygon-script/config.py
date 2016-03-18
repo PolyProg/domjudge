61ed62d779264ceb43291a021d0cba9f9cc8e545
Ubuntu <ubuntu@ip-172-31-14-71.eu-central-1.compute.internal>
2016-03-18 17:57:38 +0100

    updated config.py

--- polygon-script/config.py
+++ polygon-script/config.py
@@ -2,7 +2,7 @@
 # Local/selection contest 2015
 
 setPolygonContestId(3835)  # Needed to find the correct package file
-setDomjudgeContestId(7)
+setDomjudgeContestId(9)
 addLanguage("english")     # Call this method for every language you have statements in, and before calling add problem
 
 # Now here we list all problems:
@@ -32,7 +32,7 @@ addProblem(folder="hc2-2016-dryrun-hard",       probid="Z_dry3",   depends=True,
 
 # Main contest
 setPolygonContestId(3767)
-setDomjudgeContestId(8)
+setDomjudgeContestId(9)
 
 addProblem(folder="collective-mindsets-easy",   probid="A_mind1",  depends=False,     runtime=1 , special_runtime="1,1",   color="lemonchiffon")
 addProblem(folder="collective-mindsets-medium", probid="B_mind2",  depends=True,      runtime=1 , special_runtime="1,1",   color="yellow")
@@ -53,6 +53,7 @@ addProblem(folder="hc2-2016-walls-hard",        probid="L_wall3",  depends=True,
 addProblem(folder="jigsaw-puzzle-easy",         probid="M_jiggy1", depends=False,     runtime=15, special_runtime="22,45", color="lightcoral", offline="tests/1", compare="jiggy1")
 addProblem(folder="jigsaw-puzzle-medium",       probid="N_jiggy2", depends=True,      runtime=15, special_runtime="22,45", color="red",        offline="tests/1", compare="jiggy2")
 
-addProblem(folder="connect-the-dots-medium",    probid="O_dots2",   depends=False,    runtime=3 , special_runtime="4,6",   color="orange", compare="dots2")
-addProblem(folder="connect-the-dots-hard",      probid="P_dots3",   depends=True,     runtime=8 , special_runtime="12,16", color="darkorange", compare="dots3")
+addProblem(folder="tree-of-life-easy",          probid="O_life1",  depends=False,     runtime=3 , special_runtime="4,6",   color="lightorange", compare="life1")
+addProblem(folder="tree-of-life-medium",        probid="P_life2",  depends=True,      runtime=3 , special_runtime="4,6",   color="orange", compare="life2")
+addProblem(folder="tree-of-life-hard",          probid="Q_life3",  depends=True,      runtime=8 , special_runtime="12,16", color="darkorange", compare="life3")
 
